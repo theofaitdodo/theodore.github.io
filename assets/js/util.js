51531b33272545a2c7251820fcb1ba02681a1d1c
theo <theodore.proulx.1@ulaval.ca>
2025-10-12 13:39:28 -0400

    nouvelle carte+ animation

--- assets/js/util.js
+++ assets/js/util.js
@@ -267,9 +267,11 @@
 		// Body.
 
 			// Event: Hide panel on body click/tap.
+			/*
 				$body.on('click touchend', function(event) {
 					$this._hide(event);
 				});
+			*/
 
 			// Event: Toggle.
 				$body.on('click', 'a[href="#' + id + '"]', function(event) {
